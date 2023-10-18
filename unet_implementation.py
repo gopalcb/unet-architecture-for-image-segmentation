9c0166d072f190abb9d77f882c0de0547f5b94c5
Gopal C. Bala <gopal.cb@live.com>
2023-10-18 15:11:25 -0400

    init commit

---
+++ unet_implementation.py
@@ -0,0 +1,126 @@
+import numpy as np
+from keras.models import Model
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose
+from tensorflow.keras.optimizers import Adam
+
+LEARNING_RATE = 1e-3
+
+
+def dice_coef(y_true, y_pred, smooth = 1.0):
+    '''
+    compute dice coefficient given GT and prediction.
+    
+    params:
+    y_true: array; ground truths
+    y_pred: array; prediction
+    smooth: float
+    
+    returns:
+    dice_score: float
+    '''
+    # flatten GT and prediction
+    y = K.flatten(y_true)
+    y_hat = K.flatten(y_pred)
+    
+    # find overlap/intersection
+    intersection = K.sum(y * y_hat)
+    
+    # compute dice score
+    dice_score = (2. * intersection + smooth) / (K.sum(y) + K.sum(y_hat) + smooth)
+    
+    return dice_score
+
+
+def dice_coef_loss(y, y_pred):
+    '''
+    compute dice coefficient loss given GT and prediction.
+    
+    params:
+    y: array; ground truths
+    y_pred: array; prediction
+    
+    returns:
+    loss: float
+    '''
+    return 1 - dice_coef(y, y_pred)
+
+
+def build_unet_architecture():
+    '''
+    u-net model architecture.
+    source: https://github.com/zhixuhao/unet
+    
+    returns:
+    model: keras.model
+    '''
+    # layer 1
+    # initial input shape 256*256
+    # conv 3x3, ReLU
+    # max pool 2x2
+    inputs = Input((rows, cols, 1))
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    
+    # layer 2
+    # conv 3x3, ReLU
+    # max pool 2x2
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    
+    # layer 3
+    # conv 3x3, ReLU
+    # max pool 2x2
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    
+    # layer 4
+    # conv 3x3, ReLU
+    # max pool 2x2
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    
+    # layer 5
+    # conv 3x3, ReLU
+    # copy and crop
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+
+    # layer 6
+    # up-conv 2x2
+    # conv 3x3, ReLU
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+    
+    # layer 7
+    # up-conv 2x2
+    # conv 3x3, ReLU
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+    
+    # layer 8
+    # up-conv 2x2
+    # conv 3x3, ReLU
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+    
+    # layer 9
+    # up-conv 2x2
+    # conv 3x3, ReLU
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+
+    model.compile(optimizer=Adam(lr=LEARNING_RATE), loss=dice_coef_loss, metrics=['accuracy', dice_coef])
+
+    return model
